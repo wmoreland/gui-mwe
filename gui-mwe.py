e2e8996e029eb941f1822156ecf351aec078bb34
William Moreland <wmoreland@pm.me>
2018-07-25 13:16:05 +0200

    Tidied up code a bit

--- gui-mwe.py
+++ gui-mwe.py
@@ -4,17 +4,20 @@ import sys
 from myfunc import myfunc
 
 if __name__ == '__main__':
+
     if len(sys.argv[1:]) == 0:
         from gui import gui
         from PyQt5.QtWidgets import QApplication
         app = QApplication(sys.argv)
         ex = gui.Example()
         sys.exit(app.exec_())
+
     if len(sys.argv[1:]) > 1:
         print('This script can only handle one input at a time.')
         input = input('Please enter a single number: ')
         output = myfunc.myfunc(input)
         print('{} squared is {}'.format(input, output))
+        
     else:
         input = sys.argv[1]
         output = myfunc.myfunc(input)
